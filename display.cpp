85b2cdd38724d8ea3fe03d06e62080ce96a17dff
Matous Dzivjak <dzivjmat@fel.cvut.cz>
2017-04-20 09:06:59 +0200

    YOLO

--- display.cpp
+++ display.cpp
@@ -335,112 +335,13 @@ char *memdev="/dev/mem";
  * peripherals in the ARM Cortex-A9 physical memory address space.
  */
 #define SPILED_REG_BASE_PHYS 0x43c40000
-
-/* Valid address range for the region */
 #define SPILED_REG_SIZE      0x00004000
-
-/*
- * Byte offset of the register which controls individual LEDs
- * in the row of 32 yellow LEDs. When the corresponding bit
- * is set (value 1) then the LED is lit.
- */
 #define SPILED_REG_LED_LINE_o           0x004
-
-/*
- * The register to control 8 bit RGB components of brightness
- * of the first RGB LED
- */
 #define SPILED_REG_LED_RGB1_o           0x010
-
-/*
- * The register to control 8 bit RGB components of brightness
- * of the second RGB LED
- */
 #define SPILED_REG_LED_RGB2_o           0x014
-
-/*
- * The register which combines direct write to RGB signals
- * of the RGB LEDs, write to the keyboard scan register
- * and control of the two additional individual LEDs.
- * The direct write to RGB signals is orred with PWM
- * signal generated according to the values in previous
- * registers.
- */
 #define SPILED_REG_LED_KBDWR_DIRECT_o   0x018
-
-/*
- * Register providing access to unfiltered encoder channels
- * and keyboard return signals.
- */
 #define SPILED_REG_KBDRD_KNOBS_DIRECT_o 0x020
-
-/*
- * The register representing knobs positions as three
- * 8-bit values where each value is incremented
- * and decremented by the knob relative turning.
- */
 #define SPILED_REG_KNOBS_8BIT_o         0x024
-
-
-/*
- * The support function which returns pointer to the virtual
- * address at which starts remapped physical region in the
- * process virtual memory space.
- */
-void *map_phys_address(off_t region_base, size_t region_size, int opt_cached)
-{
-    unsigned long mem_window_size;
-    unsigned long pagesize;
-    unsigned char *mm;
-    unsigned char *mem;
-    int fd;
-
-    /*
-     * Open a device ("/dev/mem") representing physical address space
-     * in POSIX systems
-     */
-    fd = open(memdev, O_RDWR | (!opt_cached? O_SYNC: 0));
-    if (fd < 0) {
-        fprintf(stderr, "cannot open %s\n", memdev);
-        return NULL;
-    }
-
-    /*
-     * The virtual to physical address mapping translation granularity
-     * corresponds to memory page size. This call obtains the page
-     * size used by running operating system at given CPU architecture.
-     * 4kB are used by Linux running on ARM, ARM64, x86 and x86_64 systems.
-     */
-    pagesize=sysconf(_SC_PAGESIZE);
-
-    /*
-     * Extend physical region start address and size to page size boundaries
-     * to cover complete requested region.
-     */
-    mem_window_size = ((region_base & (pagesize-1)) + region_size + pagesize-1) & ~(pagesize-1);
-
-    /*
-     * Map file (in our case physical memory) range at specified offset
-     * to virtual memory ragion/area (see VMA Linux kernel structures)
-     * of the process.
-     */
-    mm = mmap(NULL, mem_window_size, PROT_WRITE|PROT_READ,
-              MAP_SHARED, fd, region_base & ~(pagesize-1));
-
-    /* Report failure if the mmap is not allowed for given file or its region */
-    if (mm == MAP_FAILED) {
-        fprintf(stderr,"mmap error\n");
-        return NULL;
-    }
-
-    /*
-     * Add offset in the page to the returned pointer for non-page-aligned
-     * requests.
-     */
-    mem = mm + (region_base & (pagesize-1));
-
-    return mem;
-}
 
 typedef unsigned char byte;
 int main(int argc, char *argv[])

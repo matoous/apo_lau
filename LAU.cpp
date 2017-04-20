329406faf55f6f7ea520d4b6df15a4caaf4f61a9
Matous Dzivjak <dzivjmat@fel.cvut.cz>
2017-04-20 09:05:08 +0200

    saaaaaaaads

---
+++ LAU.cpp
@@ -0,0 +1,5 @@
+//
+// Created by matoous on 20.04.17.
+//
+
+#include "LAU.h"
---
+++ LAU.h
@@ -0,0 +1,14 @@
+//
+// Created by matoous on 20.04.17.
+//
+
+#ifndef APO_LAU_LAU_H
+#define APO_LAU_LAU_H
+
+
+class LAU {
+
+};
+
+
+#endif //APO_LAU_LAU_H

66b2ecfa7b61b09ea5e598024c13aa8ee4d985bb
Matous Dzivjak <dzivjmat@fel.cvut.cz>
2017-04-26 00:20:06 +0200

    add files via upload?

--- app.cpp
+++ app.cpp
@@ -1,72 +1,19 @@
-/*******************************************************************
-  Simple program to check LCD functionality on MicroZed
-  based MZ_APO board designed by Petr Porazil at PiKRON
-
-  mzapo_lcdtest.c       - main and only file
-
-  (C) Copyright 2004 - 2017 by Pavel Pisa
-      e-mail:   pisa@cmp.felk.cvut.cz
-      homepage: http://cmp.felk.cvut.cz/~pisa
-      work:     http://www.pikron.com/
-      license:  any combination GPL, LGPL, MPL or BSD licenses
-
- *******************************************************************/
-
 #define _POSIX_C_SOURCE 200112L
-
-#include <sys/mman.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <malloc.h>
 #include <string.h>
 #include <byteswap.h>
 #include <getopt.h>
 #include <inttypes.h>
 #include <time.h>
-#include "par_lcd.h"
-#include "knobs.h"
-
-
-char *memdev="/dev/mem";
-
-void *map_phys_address(off_t region_base, size_t region_size, int opt_cached)
-{
-  unsigned long mem_window_size;
-  unsigned long pagesize;
-  unsigned char *mm;
-  unsigned char *mem;
-  int fd;
-
-  fd = open(memdev, O_RDWR | (!opt_cached? O_SYNC: 0));
-  if (fd < 0) {
-    fprintf(stderr, "cannot open %s\n", memdev);
-    return NULL;
-  }
-
-  pagesize=sysconf(_SC_PAGESIZE);
-
-  mem_window_size = ((region_base & (pagesize-1)) + region_size + pagesize-1) & ~(pagesize-1);
-
-  mm = mmap(NULL, mem_window_size, PROT_WRITE|PROT_READ,
-              MAP_SHARED, fd, region_base & ~(pagesize-1));
-  mem = mm + (region_base & (pagesize-1));
-
-  if (mm == MAP_FAILED) {
-    fprintf(stderr,"mmap error\n");
-    return NULL;
-  }
-
-  return mem;
-}
-
-
 #include <cmath>
-#include "pixel.h"
-#include "PPMReader.h"
-
+#include "utils/pixel.h"
+#include "utils/PPMReader.h"
+#include "perif/knobs.h"
+#include "perif/lcd_display.h"
+#include "perif/map_phys.h"
 
 /*******************************************************************
   Simple program to demostrate binary reprezentation on MicroZed
@@ -81,9 +28,6 @@ void *map_phys_address(off_t region_base, size_t region_size, int opt_cached)
       license:  any combination GPL, LGPL, MPL or BSD licenses
 
  *******************************************************************/
-
-#define _POSIX_C_SOURCE 200112L
-
 typedef unsigned char byte;
 int main(int argc, char *argv[])
 {
@@ -94,13 +38,13 @@ int main(int argc, char *argv[])
      * Setup memory mapping which provides access to the peripheral
      * registers region of RGB LEDs, knobs and line of yellow LEDs.
      */
-    knobs_mem_base = map_phys_address(SPILED_REG_BASE_PHYS, SPILED_REG_SIZE, 0);
+    knobs_mem_base = (unsigned char*)map_phys_address(SPILED_REG_BASE_PHYS, SPILED_REG_SIZE, 0);
     if (knobs_mem_base == NULL){
         printf("Knobs allocation failes.\n");
         exit(1);
     }
 
-    parlcd_mem_base = map_phys_address(PARLCD_REG_BASE_PHYS, PARLCD_REG_SIZE, 0);
+    parlcd_mem_base = (unsigned char*)map_phys_address(PARLCD_REG_BASE_PHYS, PARLCD_REG_SIZE, 0);
     if (parlcd_mem_base == NULL){
         printf("Display allocation failes.\n");
         exit(1);
@@ -108,7 +52,7 @@ int main(int argc, char *argv[])
     parlcd_hx8357_init(parlcd_mem_base);
 
     PPMReader reader(argc>1?argv[1]:"rick.ppm");
-    parlcd_write_cmd(parlcd_mem_base, 0x2c);
+    //parlcd_write_cmd(parlcd_mem_base, 0x2c);
     for (uint16_t y = 0; y < 320 ; y++) {
         for (uint16_t x = 0; x < 480 ; x++) {
             parlcd_write_data(parlcd_mem_base, reader.nextColor());
@@ -118,7 +62,6 @@ int main(int argc, char *argv[])
     unsigned char curr_LAU, curr_SC, curr_VAL, prev_VAL, new_SC, new_VAL;
 
     prev_VAL = 100;
-
     while(1){
 
         uint32_t rgb_knobs_value;
@@ -133,7 +76,32 @@ int main(int argc, char *argv[])
         new_SC = (unsigned char)(uint_val>>8)&0xFF;
         new_VAL = (unsigned char)uint_val&0xFF;
 
-        printf("station: %d color part: %d value: %d\n", (int)curr_LAU/4%num_of_stations, (int)new_SC%3, (int)curr_VAL );
+        if(prev_VAL > 200 && new_VAL < 50 && prev_VAL != new_VAL){
+            curr_VAL += 256-prev_VAL/4 + new_VAL/4;
+            if(curr_VAL > 256)
+                curr_VAL = 256;
+            prev_VAL = new_VAL;
+        }
+        else if(prev_VAL < 50 && new_VAL > 200 && prev_VAL != new_VAL){
+            curr_VAL  -= (prev_VAL/4 + 256 - new_VAL/4);
+            if(curr_VAL < 0)
+                curr_VAL = 0;
+            prev_VAL = new_VAL;
+        }
+        else if(prev_VAL > curr_VAL){
+            curr_VAL  -= (prev_VAL/4 - curr_VAL/4);
+            if(curr_VAL < 0)
+                curr_VAL = 0;
+            prev_VAL = new_VAL;
+        }
+        else if(curr_VAL > prev_VAL){
+            curr_VAL  += (curr_VAL/4 - prev_VAL/4);
+            if(curr_VAL > 256)
+                curr_VAL = 256;
+            prev_VAL = new_VAL;
+        }
+
+        printf("station: %d color part: %d value: %d\n", (int)curr_LAU/4%num_of_stations, (int)new_SC%3, (int)curr_VAL);
 
         /* Store the read value to the register controlling individual LEDs */
         *(volatile uint32_t*)(knobs_mem_base + SPILED_REG_LED_LINE_o) = 0;
---
+++ socket_rocket.h
@@ -0,0 +1,13 @@
+//
+// Created by matoous on 22.04.17.
+//
+
+#ifndef APO_LAU_SOCKET_ROCKET_H
+#define APO_LAU_SOCKET_ROCKET_H
+
+#include <netinet/in.h>
+#define SOCK_PORT 55555
+void updater(sockaddr_in);
+void sr_init();
+
+#endif //APO_LAU_SOCKET_ROCKET_H
